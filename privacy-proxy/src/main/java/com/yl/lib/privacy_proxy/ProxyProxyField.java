72eb3f0b52d205affd44045c941eef9efe69f96e
yulun <yulun@weidian.com>
2022-03-16 16:19:05 +0800

    测试高德地图，demo增加黑名单样例

--- privacy-proxy/src/main/java/com/yl/lib/privacy_proxy/ProxyProxyField.java
+++ privacy-proxy/src/main/java/com/yl/lib/privacy_proxy/ProxyProxyField.java
@@ -1,17 +1,14 @@
 package com.yl.lib.privacy_proxy;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
+import androidx.annotation.Keep;
 
 /**
  * @author yulun
  * @since 2022-03-03 19:42
  * kotlin里的伴生对象 定义的常量编译后都是private
  */
-
+@Keep
 public class ProxyProxyField {
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public static final String  proxySerial = PrivacyProxyCall.Proxy.getSerial();
+    public static final String proxySerial = PrivacyProxyCall.Proxy.getSerial();
 }
