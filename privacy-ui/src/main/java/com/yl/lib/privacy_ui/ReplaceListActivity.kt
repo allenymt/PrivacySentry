9aeff65c42feca62e7a15c3637b6ee1cb3d72a1f
yulun <yulunandroid@gmail.com>
2022-07-01 14:40:25 +0800

    code

--- privacy-ui/src/main/java/com/yl/lib/privacy_ui/ReplaceListActivity.kt
+++ privacy-ui/src/main/java/com/yl/lib/privacy_ui/ReplaceListActivity.kt
@@ -2,11 +2,15 @@ package com.yl.lib.privacy_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ProgressBar
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.RecyclerView
 import com.yl.lib.sentry.hook.PrivacySentry
+import java.io.ByteArrayOutputStream
+import java.io.File
 import java.io.FileInputStream
+import java.io.InputStream
 import java.lang.Exception
 
 /**
@@ -38,5 +42,55 @@ class ReplaceListActivity : AppCompatActivity() {
         }catch (e:Exception){
             e.printStackTrace()
         }
+    }
+
+    private fun loadConfigFile(filePath: String): String? {
+        try {
+            var file = File(filePath + File.separator + "CONFIG_JSON_NAME")
+            if (file.exists()) {
+                val data = convertStreamToByte(FileInputStream(file))
+                if (data != null) {
+                    return String(data)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return ""
+    }
+
+    fun convertStreamToByte(inputStream: InputStream?): ByteArray? {
+        if (inputStream == null) {
+            return null
+        }
+        var bos: ByteArrayOutputStream? = null
+        try {
+            bos = ByteArrayOutputStream()
+            val buffer = ByteArray(2 * 1024)
+            var read = -1
+            while (inputStream.read(buffer)?.also { read = it } != -1) {
+                bos.write(buffer, 0, read)
+            }
+            return bos.toByteArray()
+        } catch (e: java.lang.Exception) {
+            Log.e("error:", e.toString())
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close()
+                } catch (e2: java.lang.Exception) {
+                }
+            }
+        }
+        return null
+    }
+
+    private fun convertStreamToString(inputStream: InputStream?): String? {
+        var result: String? = ""
+        val data: ByteArray? = convertStreamToByte(inputStream)
+        if (data != null) {
+            result = String(data)
+        }
+        return result
     }
 }
