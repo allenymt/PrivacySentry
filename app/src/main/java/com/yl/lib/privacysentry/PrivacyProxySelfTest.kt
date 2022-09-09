3f74e4bb4aa3f083d5de4f0bd6bdee4830f383a2
yulun <yulunandroid@gmail.com>
2022-09-09 12:05:46 +0800

    update demo version

--- app/src/main/java/com/yl/lib/privacysentry/PrivacyProxySelfTest.kt
+++ app/src/main/java/com/yl/lib/privacysentry/PrivacyProxySelfTest.kt
@@ -1,10 +1,15 @@
 package com.yl.lib.privacysentry
 
 import android.app.ActivityManager
+import android.content.ContentResolver
+import android.os.Build
+import android.provider.Settings
 import androidx.annotation.Keep
 import com.yl.lib.privacy_annotation.MethodInvokeOpcode
 import com.yl.lib.privacy_annotation.PrivacyClassProxy
 import com.yl.lib.privacy_annotation.PrivacyMethodProxy
+import com.yl.lib.sentry.hook.PrivacySentry
+import com.yl.lib.sentry.hook.util.PrivacyProxyUtil
 import com.yl.lib.sentry.hook.util.PrivacyProxyUtil.Util.doFilePrinter
 
 /**
@@ -17,19 +22,66 @@ class PrivacyProxySelfTest {
     @PrivacyClassProxy
     @Keep
     object Proxy {
-        // 这个方法的注册放在了PrivacyProxyCall2中，提供了一个java注册的例子
+
+        var objectAndroidIdLock = Object()
         @PrivacyMethodProxy(
-            originalClass = ActivityManager::class,
-            originalMethod = "getRunningTasks",
-            originalOpcode = MethodInvokeOpcode.INVOKEVIRTUAL
+            originalClass = Settings.Secure::class,
+            originalMethod = "getString",
+            originalOpcode = MethodInvokeOpcode.INVOKESTATIC
         )
         @JvmStatic
-        fun getRunningTasks123(
-            manager: ActivityManager,
-            maxNum: Int
-        ): List<ActivityManager.RunningTaskInfo?>? {
-           doFilePrinter("getRunningTasks", "获取当前运行中的任务", "")
-            return manager.getRunningTasks(maxNum)
+        fun getString(contentResolver: ContentResolver?, type: String?): String? {
+            var key = "Secure-getString-$type"
+            if (!"android_id".equals(type)) {
+                return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+                    Settings.Secure.getString(
+                        contentResolver,
+                        type
+                    )
+                } else {
+                    ""
+                }
+            }
+            if (PrivacySentry.Privacy.getBuilder()?.isVisitorModel() == true) {
+                doFilePrinter(
+                    "getString",
+                    "系统信息",
+                    args = type,
+                    bVisitorModel = true
+                )
+                return ""
+            }
+            synchronized(objectAndroidIdLock) {
+                var hasValue = PrivacyProxyUtil.Util.hasReadStaticParam(key)
+                if (!hasValue) {
+                    doFilePrinter("getString", "系统信息", args = type)
+                    var result = ""
+                    try {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+                            result = Settings.Secure.getString(
+                                contentResolver,
+                                type
+                            )
+                        }
+                    } catch (e: Throwable) {
+                        //不管有没有申请，部分机子直接跑异常
+                        throw e
+                    } finally {
+                        PrivacyProxyUtil.Util.putCacheStaticParam(result ?: "", key)
+                    }
+                    return result
+                } else {
+                    doFilePrinter(
+                        "getString",
+                        "系统信息",
+                        args = type,
+                        bCache = true
+                    )
+                    return PrivacyProxyUtil.Util.getCacheStaticParam("", key)
+                }
+            }
         }
     }
+
+
 }
