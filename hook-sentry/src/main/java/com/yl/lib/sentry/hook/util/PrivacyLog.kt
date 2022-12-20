ae00f04b1c487c965267969bce785102fb2019ca
yulun <yulunandroid@gmail.com>
2022-12-20 17:52:09 +0800

    优化下日志输出逻辑

--- hook-sentry/src/main/java/com/yl/lib/sentry/hook/util/PrivacyLog.kt
+++ hook-sentry/src/main/java/com/yl/lib/sentry/hook/util/PrivacyLog.kt
@@ -12,7 +12,7 @@ class PrivacyLog {
         private const val TAG = "PrivacyOfficer"
 
         fun e(msg: String) {
-            if (PrivacySentry.Privacy.isDebug()) {
+            if (PrivacySentry.Privacy.isDebug() || PrivacySentry.Privacy.inDangerousState()) {
                 android.util.Log.e(TAG, msg)
             }
         }
