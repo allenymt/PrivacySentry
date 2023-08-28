4409889c6e9b3b2f830084d82702be00edeed93c
yulun <yulunandroid@gmail.com>
2023-08-28 17:55:17 +0800

    try fix

--- plugin-sentry/src/main/java/com/yl/lib/plugin/sentry/transform/booster/processor/PrivacyAssetsProcessor.kt
+++ plugin-sentry/src/main/java/com/yl/lib/plugin/sentry/transform/booster/processor/PrivacyAssetsProcessor.kt
@@ -19,11 +19,12 @@ class PrivacyAssetsProcessor : VariantProcessor {
         // privacy插件不支持library单独引用，只支持application引用
         if (variant is ApplicationVariant) {
             "PrivacyAssetsProcessor ${variant.name}".privacyPrintln()
+            var variantName = variant.name.capitalize()
             var moveTask = variant.project.tasks.create(
-                "Privacy${variant.name}AssetsTask",
+                "Privacy${variantName}AssetsTask",
                 PrivacyMoveAssetsTask::class.java
             )
-            "PrivacyAssetsProcessor ${variant.name} MoveAssetsTask is $moveTask".privacyPrintln()
+            "PrivacyAssetsProcessor $variantName MoveAssetsTask is $moveTask".privacyPrintln()
             var privacyExtension = variant.project.extensions.findByType(
                 PrivacyExtension::class.java
             )
@@ -39,12 +40,12 @@ class PrivacyAssetsProcessor : VariantProcessor {
                         ignoreCase = true
                     ) && task.transform is PrivacyHookTransform
                 }
-            variant.project.logger.info("project MoveAssetsTask finalizedBy privacySentryTask  is $privacySentryTask , MoveAssetsTask is $moveTask ${variant.name} after fileName is ${moveTask.fileName} ")
+            variant.project.logger.info("project MoveAssetsTask finalizedBy privacySentryTask  is $privacySentryTask , MoveAssetsTask is $moveTask $variantName after fileName is ${moveTask.fileName} ")
             // 在任务结束之后执行指定的 Task, 也就是mergeAssetsTask执行完后，执行moveTask，把我们代理的api列表同步到assets目录下
             variant.project.logger.info("AssetsTask is ${variant.mergeAssetsProvider.get().name}")
             privacySentryTask.finalizedBy(moveTask)
             // moveTask 在privacySentryTask执行
-            moveTask.mustRunAfter(variant.mergeAssetsProvider.get())
+//            moveTask.mustRunAfter(variant.mergeAssetsProvider.get())
         }
     }
 
