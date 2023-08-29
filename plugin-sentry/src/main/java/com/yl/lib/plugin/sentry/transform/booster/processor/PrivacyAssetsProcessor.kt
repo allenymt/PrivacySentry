c98fa033b42d89de97dd1e6a48cadbe075c87596
yulun <yulunandroid@gmail.com>
2023-08-29 11:07:57 +0800

    增加日志

--- plugin-sentry/src/main/java/com/yl/lib/plugin/sentry/transform/booster/processor/PrivacyAssetsProcessor.kt
+++ plugin-sentry/src/main/java/com/yl/lib/plugin/sentry/transform/booster/processor/PrivacyAssetsProcessor.kt
@@ -3,6 +3,7 @@ package com.yl.lib.plugin.sentry.transform.booster.processor
 import com.android.build.gradle.api.ApplicationVariant
 import com.android.build.gradle.api.BaseVariant
 import com.android.build.gradle.internal.pipeline.TransformTask
+import com.didiglobal.booster.gradle.getTaskName
 import com.didiglobal.booster.gradle.project
 import com.didiglobal.booster.task.spi.VariantProcessor
 import com.yl.lib.plugin.sentry.extension.PrivacyExtension
@@ -24,6 +25,10 @@ class PrivacyAssetsProcessor : VariantProcessor {
                 "Privacy${variantName}AssetsTask",
                 PrivacyMoveAssetsTask::class.java
             )
+           var findTask =  variant.project.tasks.findByName(
+                variant.getTaskName("Privacy", "AssetsTask")
+            )
+            "PrivacyAssetsProcessor findTask  is $findTask".privacyPrintln()
             "PrivacyAssetsProcessor $variantName MoveAssetsTask is $moveTask".privacyPrintln()
             var privacyExtension = variant.project.extensions.findByType(
                 PrivacyExtension::class.java
@@ -40,9 +45,9 @@ class PrivacyAssetsProcessor : VariantProcessor {
                         ignoreCase = true
                     ) && task.transform is PrivacyHookTransform
                 }
-            variant.project.logger.info("project MoveAssetsTask finalizedBy privacySentryTask  is $privacySentryTask , MoveAssetsTask is $moveTask $variantName after fileName is ${moveTask.fileName} ")
+            variant.project.logger.info("PrivacyAssetsProcessor project MoveAssetsTask finalizedBy privacySentryTask  is $privacySentryTask , MoveAssetsTask is $moveTask $variantName after fileName is ${moveTask.fileName} ")
             // 在任务结束之后执行指定的 Task, 也就是mergeAssetsTask执行完后，执行moveTask，把我们代理的api列表同步到assets目录下
-            variant.project.logger.info("AssetsTask is ${variant.mergeAssetsProvider.get().name}")
+            variant.project.logger.info("PrivacyAssetsProcessor AssetsTask is ${variant.mergeAssetsProvider.get().name}")
             privacySentryTask.finalizedBy(moveTask)
             moveTask.dependsOn(privacySentryTask)
             // moveTask 在privacySentryTask执行
