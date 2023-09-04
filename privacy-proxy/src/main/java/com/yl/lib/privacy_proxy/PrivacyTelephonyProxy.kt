f6b6d971a30f2bf7def1a82399e41b410ad0bf97
yulun <yulunandroid@gmail.com>
2023-09-04 09:59:19 +0800

    修改注释

--- privacy-proxy/src/main/java/com/yl/lib/privacy_proxy/PrivacyTelephonyProxy.kt
+++ privacy-proxy/src/main/java/com/yl/lib/privacy_proxy/PrivacyTelephonyProxy.kt
@@ -100,7 +100,6 @@ open class PrivacyTelephonyProxy {
         var objectDeviceIdLock = Object()
 
 
-        // 配置代理拦截获取设备ID
         @PrivacyMethodProxy(
             originalClass = TelephonyManager::class,
             originalMethod = "getDeviceId",
@@ -109,7 +108,7 @@ open class PrivacyTelephonyProxy {
         @JvmStatic
         fun getDeviceId(manager: TelephonyManager): String? {
             var key = "TelephonyManager-getDeviceId"
-            // 在用户同意协议之前，拦截获取
+            // 在用户同意协议之前，不可进行获取
             if (PrivacySentry.Privacy.inDangerousState()) {
                 PrivacyProxyUtil.Util.doFilePrinter(key, "IMEI-getDeviceId()", bVisitorModel = true)
                 return ""
@@ -119,7 +118,7 @@ open class PrivacyTelephonyProxy {
                 return ""
             }
 
-            // 如果没有获得电话权限，拦截获取
+            // 如果没有获得电话权限，不可进行获取
             if (!PrivacyProxyUtil.Util.checkPermission(Manifest.permission.READ_PHONE_STATE)) {
                 PrivacyProxyUtil.Util.doFilePrinter(key, "IMEI-getDeviceId()-无权限")
                 return ""
