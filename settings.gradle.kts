2264e1679212469ceb9fad13e2c55817967e0925
yulun <yulun@weidian.com>
2025-05-16 10:38:13 +0800

    fix  error

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,9 +9,11 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
         google()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
